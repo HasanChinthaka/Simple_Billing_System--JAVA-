f32c0cbdadb70cd805cd5dcdc2dc42e4e3d0ff1b
Hasan Chinthaka <pgrhasanchinthaka@gmail.com>
2023-12-09 16:45:05 +0530

    Create simple Billing system

---
+++ Simple_Billing_System/src/main/java/com/mycompany/simple_billing_system/Simple_Billing_System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.simple_billing_system;
+
+/**
+ *
+ * @author User
+ */
+public class Simple_Billing_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
